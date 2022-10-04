d95e2ed04c42580539bc862bd843261103430fa0
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-10-04 10:58:13 +0330

    Include status for profile card

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -14,6 +14,19 @@ export const Profile = ({ celebrity }: ProfileProps) => {
     account => account.type === 'TWITTER',
   )
 
+  function HandleColorStatus() {
+    switch(celebrity.status) {
+      case 'BAD':
+        return 'bg-red-500'
+      case 'GOOD':
+        return 'bg-green-500'
+      case 'UGLY':
+        return 'bg-gray-500'
+      default:
+        return 'bg-transparent'
+    }
+  }
+
   return (
     <div className="sm:p-10 min-h-screen">
       <div className="flex flex-col rounded-md bg-neutral-100">
@@ -37,11 +50,14 @@ export const Profile = ({ celebrity }: ProfileProps) => {
             />
           </svg>
         </button>
-        <img
-          src={celebrity.imageUrl}
-          alt={celebrity.name}
-          className="w-1/2 rounded-full overflow-hidden mx-auto"
-        />
+        <div className='relative w-1/2 mx-auto'>
+          <img
+            src={celebrity.imageUrl}
+            alt={celebrity.name}
+            className="w-full h-full rounded-full overflow-hidden object-cover"
+          />
+          <div className={`z-10 absolute bottom-0 right-0 w-5 h-5 rounded-full ${HandleColorStatus()}`}>{' '}</div>
+        </div>
         <h1 className="text-2xl text-center mt-5 text-neutral-800">
           {celebrity.name}
         </h1>
